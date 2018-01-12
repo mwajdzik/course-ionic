d73d5288a21a6012996b7066b57f1df149a2c297
Wajdzik Maciej <wajdzik.maciej@gmail.com>
2018-01-12 07:36:03 +0100

    Ionic. Quotes App. Pages.

--- X-Quotes/src/app/app.module.ts
+++ X-Quotes/src/app/app.module.ts
@@ -5,12 +5,20 @@ import {SplashScreen} from '@ionic-native/splash-screen';
 import {StatusBar} from '@ionic-native/status-bar';
 
 import {MyApp} from './app.component';
-import {HomePage} from '../pages/home/home';
+import {FavoritesPage} from "../pages/favorites/favorites";
+import {LibraryPage} from "../pages/library/library";
+import {QuotesPage} from "../pages/quotes/quotes";
+import {SettingsPage} from "../pages/settings/settings";
+import {QuotePage} from "../pages/quote/quote";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    LibraryPage,
+    QuotesPage,
+    QuotePage,
+    SettingsPage,
+    FavoritesPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +27,11 @@ import {HomePage} from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    LibraryPage,
+    QuotesPage,
+    QuotePage,
+    SettingsPage,
+    FavoritesPage
   ],
   providers: [
     StatusBar,
